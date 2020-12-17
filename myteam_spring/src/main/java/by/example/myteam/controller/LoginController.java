b4e2ef0f62a2515298df6a993d58e5e9680485f1
bdrem <bdremb@gmail.com>
2020-12-17 15:32:01 +0300

    rename controller v14

--- myteam_spring/src/main/java/by/example/myteam/controller/LoginController.java
+++ myteam_spring/src/main/java/by/example/myteam/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ViewController {
+public class LoginController {
     @RequestMapping("/about")
     public String about() {
         return "about";
