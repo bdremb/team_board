4a2fde0fbfafafe9d1b6f29b5eb6e73b36e160d6
bdrem <bdremb@gmail.com>
2021-01-06 18:26:41 +0300

    ready first testMethod, testGetAllPersons

--- team_board/src/test/java/by/example/team_board/dao/PersonDAOImplTest.java
+++ team_board/src/test/java/by/example/team_board/dao/PersonDAOImplTest.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PersonDAOImplTest extends PersonDAOImpl {
 
-
     public PersonDAOImplTest(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
--- team_board/src/test/java/by/example/team_board/service/PersonServiceTest.java
+++ team_board/src/test/java/by/example/team_board/service/PersonServiceTest.java
@@ -1,31 +1,45 @@
 package by.example.team_board.service;
 
 import by.example.team_board.dao.PersonDAOImplTest;
+import by.example.team_board.entity.ExtraInfo;
 import by.example.team_board.entity.Person;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class PersonServiceTest {
-    private PersonDAOImplTest personDAO;
-
+    private static PersonDAOImplTest personDAOImplTest;
+    private static SessionFactory factory;
 
     @BeforeClass
     public static void setUp() {
-
+        factory = new Configuration().configure("test.hibernate.cfg.xml")
+                .addAnnotatedClass(Person.class).addAnnotatedClass(ExtraInfo.class).buildSessionFactory();
+        factory.getCurrentSession().beginTransaction();
+        personDAOImplTest = new PersonDAOImplTest(factory);
     }
 
     @Test
-    @Transactional
     public void testGetAllPersons() {
-        List<Person> personList = personDAO.getAllPersons();
-        Assert.assertEquals(personList, null);
-       // Assert.assertEquals(expected, actual);  //ожидаемое, актуальное значения
+        List<Person> personList = personDAOImplTest.getAllPersons();
+        for (Person person : personList) {
+            System.out.println(person.getLogin() + " " + person.getGender());
+        }
+        Assert.assertEquals(personList.get(0).getName(), "Sergey");
+        Assert.assertEquals(personList.get(1).getExtraInfo().getAge(), 12);
+    }
+
+    @AfterClass
+    public static void finished() {
+        factory.getCurrentSession().getTransaction().commit();
+        factory.close();
     }
 }
 
