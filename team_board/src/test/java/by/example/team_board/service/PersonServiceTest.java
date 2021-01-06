762ab5b11cab59fb30a534bc2dc7feba4828afae
bdrem <bdremb@gmail.com>
2021-01-06 19:51:33 +0300

    ready first testMethods -  getPerson, saveAndDeletePersons

--- team_board/src/test/java/by/example/team_board/service/PersonServiceTest.java
+++ team_board/src/test/java/by/example/team_board/service/PersonServiceTest.java
@@ -15,25 +15,47 @@ import java.util.List;
 
 @Service
 public class PersonServiceTest {
-    private static PersonDAOImplTest personDAOImplTest;
     private static SessionFactory factory;
+    private static PersonService personService;
+    private static Person person;
 
     @BeforeClass
     public static void setUp() {
         factory = new Configuration().configure("test.hibernate.cfg.xml")
                 .addAnnotatedClass(Person.class).addAnnotatedClass(ExtraInfo.class).buildSessionFactory();
         factory.getCurrentSession().beginTransaction();
-        personDAOImplTest = new PersonDAOImplTest(factory);
+        personService = new PersonServiceImpl(new PersonDAOImplTest(factory));
+        person = new Person();
+        person.setExtraInfo(new ExtraInfo());
+        person.setName("Mikhail");
+        person.setLogin("login123123123");
     }
 
     @Test
     public void testGetAllPersons() {
-        List<Person> personList = personDAOImplTest.getAllPersons();
+        List<Person> personList = personService.getAllPersons();
         for (Person person : personList) {
             System.out.println(person.getLogin() + " " + person.getGender());
         }
         Assert.assertEquals(personList.get(0).getName(), "Sergey");
         Assert.assertEquals(personList.get(1).getExtraInfo().getAge(), 12);
+    }
+
+    @Test
+    public void testGetPerson() {
+        Person testPerson = personService.getPerson(3);
+        Assert.assertEquals(testPerson.getSurname(), "Sidorova");
+        Assert.assertEquals(testPerson.getExtraInfo().getPhoneNumber(), "998877664");
+    }
+
+    @Test
+    public void testSaveAndDeletePerson() {
+        boolean result = personService.savePerson(person);
+        Assert.assertTrue(result);
+        boolean result2 = personService.savePerson(person);
+        Assert.assertFalse(result2);
+        personService.deletePerson(person.getId());
+        Assert.assertNull(personService.getPerson(person.getId()));
     }
 
     @AfterClass
